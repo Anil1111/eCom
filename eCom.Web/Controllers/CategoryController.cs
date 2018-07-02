e5edb66d291438ad48048ba7abead4402c15f974
Sajjad Gul <sajjadarifgul@gmail.com>
2018-07-02 14:23:41 +0500

    Catgeory Creation Success

---
+++ eCom.Web/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using eCom.Web.Entities;
+using eCom.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCom.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            return View();
+        }
+    }
+}
