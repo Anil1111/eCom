5f62a7ecf0c1ea5b7abfa0e7f3ee0a673df71d36
Sajjad Gul <sajjadarifgul@gmail.com>
2018-07-09 16:57:51 +0500

    URLHelpers

---
+++ eCom.Web/Helpers/URLHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCom.Web.Helpers
+{
+    public static class eComURLHelper
+    {
+        public static string Categories(this UrlHelper helper, int? pageNo, int? items)
+        {
+            string routeURL = string.Empty;
+
+            routeURL = helper.RouteUrl("Categories", new
+            {
+                pageNo = pageNo,
+                items = items
+            });
+
+            routeURL = HttpUtility.UrlDecode(routeURL, System.Text.Encoding.UTF8);
+
+            return routeURL.ToLower();
+        }
+    }
+}
